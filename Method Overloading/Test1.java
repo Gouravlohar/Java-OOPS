effb2398ab29860a7488a38d97cb09a7f8fe8b92
Gourav Lohar <55839674+Gouravlohar@users.noreply.github.com>
2023-12-09 06:42:52 +0530

    Create Test1.java

---
+++ Method Overloading/Test1.java
@@ -0,0 +1,14 @@
+class Test1{
+
+	void show(int a){
+	System.out.println("a method class");
+}
+
+	void show(){
+	System.out.println("b method class");}
+	
+public static void main(String[] args){
+Test1 obj=new Test1();
+obj.show();
+}
+}
